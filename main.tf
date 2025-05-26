ee47b149976671b1e3415ae684f3b3ae42706693
Attila Sovak <attila.sovak@gmail.com>
2025-05-26 14:11:01 +0200

    feat(pyroscope): add init files

---
+++ main.tf
@@ -0,0 +1,33 @@
+/**
+ * # AWS EKS Universal Addon Terraform module
+ *
+ * A Terraform module to deploy the universal addon on Amazon EKS cluster.
+ *
+ * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/validate.yaml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/pre-commit.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/pre-commit.yaml)
+ */
+
+locals {
+  addon = {
+    name = "universal-addon"
+
+    helm_chart_name    = "raw"
+    helm_chart_version = "0.1.0"
+    helm_repo_url      = "https://lablabs.github.io"
+  }
+
+  # FIXME config: add addon IRSA configuration here or remove if not needed
+  addon_irsa = {
+    (local.addon.name) = {
+      # FIXME config: add default IRSA overrides here or leave empty if not needed, but make sure to keep at least one key
+    }
+  }
+
+  addon_values = yamlencode({
+    # FIXME config: add default values here or leave empty if not needed
+  })
+
+  addon_depends_on = [
+    # FIXME config: add dependencies here, i.e. CRDs, or leave empty if not needed
+  ]
+}
